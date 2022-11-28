fb4dacbbb7ba7a12a708ad240d76a0ad0c0ba41c
Manu <emmanuel@farcis.fr>
2022-11-28 21:22:39 +0100

    add nvimtree.lua

---
+++ lua/config/nvimtree.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+function M.setup()
+  require("nvim-tree").setup {
+    disable_netrw = true,
+    hijack_netrw = true,
+    view = {
+      number = true,
+      relativenumber = true,
+      side = "left",
+    },
+    update_cwd = true,
+    open_on_setup = true,
+    open_on_setup_file = true,
+    actions = {
+      open_file = {
+        resize_window = true,
+      },
+    },
+    update_focused_file = {
+      enable = true,
+      update_cwd = true,
+    },
+    filters = {
+      dotfiles = false,
+    },
+    diagnostics = {
+      enable = true,
+    },
+    git = {
+        enable = true,
+        ignore = false,
+        show_on_dirs = true,
+        timeout = 400,
+      },
+  }
+end
+
+return M
